bc3c0b31bbb34a3435fb1c3c778e1886bbf39d10
imanimen <imanimen81@gmail.com>
2024-10-02 00:27:12 +0330

    feat(match): checking the user input with the secret number

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use std::io;
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+// use std::io::stdin  // another way to call the method of the standard library
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
+    // dividing lines for more redability
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
 
-    println!("You guessed: {}", guess)
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
+    println!("The secret number was: {secret_number}")
 }
