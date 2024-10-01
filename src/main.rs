5603de0449d775fc6af01a2f32b6fb42458dcf58
imanimen <imanimen81@gmail.com>
2024-10-02 00:30:25 +0330

    chore(loop): infinite loop for the game

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     // println!("The secret number is: {secret_number}");
+    loop {
+            
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        // dividing lines for more redability
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    // dividing lines for more redability
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!") 
+        }
     }
-    println!("The secret number was: {secret_number}")
 }
