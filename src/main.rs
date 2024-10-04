8c4353e59008546f43efa1f692e73acb64e02f79
iman <imanimen81@gmail.com>
2024-10-04 18:46:06 +0330

    final(final commit)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 use std::{cmp::Ordering, io};
 
 use rand::Rng;
-// use std::io::stdin  // another way to call the method of the standard library
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // println!("The secret number is: {secret_number}");
     loop {
             
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
-        // dividing lines for more redability
         io::stdin()
             .read_line(&mut guess)
             .expect("failed to read line");
